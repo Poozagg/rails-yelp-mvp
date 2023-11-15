8ed60d94845532a98e637f2499cce745944c1469
Pooja Gurung <pooja_grg07@hotmail.com>
2023-11-15 16:57:24 +0000

    done the  restraurant (index & show pages). done the reviews (new & shown on restaurant show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+category_options = ["chinese", "italian", "japanese", "french", "belgian"]
+5.times do
+  restaurant = Restaurant.create(name: Faker::Name.unique.name, address: Faker::Address.full_address, category: category_options.sample, phone_number: Faker::PhoneNumber.phone_number_with_country_code)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
